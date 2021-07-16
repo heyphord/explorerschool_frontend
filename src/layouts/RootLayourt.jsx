0ac40644a5cc9335f35ac675d00775586fdda6c2
heyphord <apps.heyphord@gmail.com>
2021-07-16 09:27:09 +0000

    page routes done

--- src/layouts/RootLayourt.jsx
+++ src/layouts/RootLayourt.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import TutorsPage from "../views/TutorsPage";
+import TutorDetailsPage from "../views/TutorDetailsPage";
+import AddTutorPage from "../views/AddTutorPage";
+import EditTutorPage from "../views/EditTutorPage";
+import AddStudentPage from "../views/AddStudentPage";
+import EditStudentPage from "../views/EditStudentPage";
 
 
 export default function RootLayout() {
@@ -10,6 +15,11 @@ export default function RootLayout() {
       <Switch>
         
         <Route path="/website/tutors" component={TutorsPage} />
+        <Route path="/website/tutor" component={TutorDetailsPage} />
+        <Route path="/website/add-tutor" component={AddTutorPage} />
+        <Route path="/website/edit-tutor" component={EditTutorPage} />
+        <Route path="/website/add-student" component={AddStudentPage} />
+        <Route path="/website/edit-student" component={EditStudentPage} />
        
         <Redirect to="/website/tutors" />
       </Switch>
