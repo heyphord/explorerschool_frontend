a8909faf29b816aa9c28f178f2808c41bde578c4
heyphord <apps.heyphord@gmail.com>
2021-07-16 11:03:02 +0000

    Added a home page

--- src/layouts/RootLayourt.jsx
+++ src/layouts/RootLayourt.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 
+import HomePage from "../views/HomePage";
 import TutorsPage from "../views/TutorsPage";
 import TutorDetailsPage from "../views/TutorDetailsPage";
 import AddTutorPage from "../views/AddTutorPage";
@@ -20,6 +21,7 @@ export default function RootLayout() {
             <Header/>
             <Switch>
 
+                <Route path="/website/home" component={HomePage} />
                 <Route path="/website/tutors" component={TutorsPage} />
                 <Route path="/website/students" component={StudentsPage} />
                 <Route path="/website/tutor" component={TutorDetailsPage} />
@@ -28,7 +30,7 @@ export default function RootLayout() {
                 <Route path="/website/add-student" component={AddStudentPage} />
                 <Route path="/website/edit-student" component={EditStudentPage} />
 
-                <Redirect to="/website/tutors" />
+                <Redirect to="/website/home" />
             </Switch>
 
         </div>
---
+++ src/views/HomePage.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col,
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+
+export default function HomePage() {
+
+    return (
+        <>
+            <Container>
+                <Row className="justify-content-center m-3">
+                    <Col xs="3">
+                        <h1>HOME PAGE </h1>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col xs="3">
+                        <Card>
+                            <CardImg top width="100%" src="https://image.shutterstock.com/image-photo/high-school-tutor-giving-male-600w-1127431355.jpg" alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">View Tutors</CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2 text-muted">View all tutors</CardSubtitle>
+                                <CardText>Click the button below to see all available tutors.</CardText>
+                                <Button href="/website/tutots" >View Tutors</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col xs="3">
+                        <Card>
+                            <CardImg top width="100%" src="https://image.shutterstock.com/image-photo/african-american-young-adult-dreads-600w-1670707486.jpg" alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">View Students</CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2 text-muted">View all Students</CardSubtitle>
+                                <CardText>Click the button below to see all students.</CardText>
+                                <Button href="/website/tutots" >View Students</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                        
+                </Row>
+                <Row className="justify-content-center mt-3">
+                    <Col xs="3">
+                        <Card>
+                            <CardImg top width="100%" src="https://image.shutterstock.com/image-photo/young-indian-teacher-talk-speak-600w-1916410994.jpg" alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">Add New Tutor</CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2 text-muted">New tutor</CardSubtitle>
+                                <CardText>Click the button below to add a new tutor.</CardText>
+                                <Button href="/website/tutots" >Add new tutor</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col xs="3">
+                        <Card>
+                            <CardImg top width="90%" src="https://image.shutterstock.com/image-photo/homeschool-asian-little-young-girl-600w-1739402348.jpg" alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">Add New Student </CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2 text-muted">New student</CardSubtitle>
+                                <CardText>Click the button below to add a new student.</CardText>
+                                <Button href="/website/tutots" >Add new student</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    
+                </Row>
+
+            </Container>
+        </>
+    )
+}
