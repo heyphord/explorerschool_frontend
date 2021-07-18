923cf632bc343345e472eda2b8c87783d451fffc
heyphord <apps.heyphord@gmail.com>
2021-07-18 17:17:28 +0000

    view student and delete student done

--- src/views/AddStudentPage.js
+++ src/views/AddStudentPage.js
@@ -1,8 +1,93 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input, Container ,Spinner } from 'reactstrap';
+import {  useToasts } from 'react-toast-notifications';
 
-export default function AddStudentPage(){
+//redux
+import { useSelector , useDispatch } from "react-redux";
+import { createStudentMiddleware } from "../redux/middleware/StudentMiddleware";
+import { createStudentError, createStudentSuccess ,resetActionType } from "../redux/reducers/StudentReducer";
 
-    return(
-        <h1>AddStudentPage</h1>
+export default function AddStudentPage() {
+
+    //redux
+    const studentState = useSelector( state=> state.student);
+    const dispatch = useDispatch();
+
+    //toast
+    const { addToast } = useToasts();
+
+    const [firstName, setFirstName] = React.useState();
+    const [lastName, setLastName] = React.useState();
+    const [email, setEmail] = React.useState();
+    const [department, setDepartment] = React.useState();
+
+    const onSubmit = ()=>{
+
+        if(!firstName || !lastName || !email || !department){
+
+            addToast("Fields cannot be empty", { appearance: 'error' });
+            return;
+
+        }else if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+
+            addToast("Please enter a valid email address", { appearance: 'error' })
+            return;
+          }
+
+          dispatch( createStudentMiddleware( firstName,lastName, email ,department))
+    }
+
+    React.useEffect(() => {
+        if (studentState.ACTION_TYPE === createStudentSuccess.toString()) {
+          
+            addToast("Student Added successfully", { appearance: 'success' });
+
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setDepartment("");
+    
+        } else if (studentState.ACTION_TYPE === createStudentError.toString()) {
+          addToast("Error Adding student", { appearance: 'error' })
+    
+        }
+        dispatch( resetActionType())
+    
+    
+      }, [studentState.ACTION_TYPE]);
+
+    return (
+        <>
+            <Container>
+                <h1>Add a new student</h1>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleFname">First Name</Label>
+                        <Input  value={firstName} onChange={ e => setFirstName(e.target.value)} type="text" name="exampleFname" id="exampleFname" placeholder="enter first name" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleLname">Last Name</Label>
+                        <Input value={lastName} onChange={ e => setLastName(e.target.value)} type="text" name="exampleLname" id="exampleLname" placeholder="Enter last name" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleEmail">staff email</Label>
+                        <Input value={email} onChange={ e => setEmail(e.target.value)} type="email" name="email" id="exampleEmail" placeholder="Enter email" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleEmail">Department</Label>
+                        <Input value={department} onChange={ e => setDepartment(e.target.value)} type="email" name="department" id="exampleEmail" placeholder="Enter department" />
+                    </FormGroup>
+                    
+                    
+                    <Button onClick={onSubmit}>
+                        {studentState.isCreateStudentLoading?
+                        <Spinner children=""/>:
+                        "Add new student"
+                    }
+                    </Button>
+                </Form>
+            </Container>
+        </>
     )
 }
--- src/views/StudentsPage.js
+++ src/views/StudentsPage.js
@@ -1,20 +1,44 @@
 import React from "react";
-import { Table } from 'reactstrap';
+import { Table ,Button, Modal, ModalHeader, ModalBody, ModalFooter, Spinner} from 'reactstrap';
 
 //redux
 import { useSelector , useDispatch } from "react-redux";
-import { getStudentsMiddleware } from "../redux/middleware/StudentMiddleware";
+import { deleteStudentMiddleware, getStudentsMiddleware } from "../redux/middleware/StudentMiddleware";
+import { deleteStudentSuccess } from "../redux/reducers/StudentReducer";
+
+import { useToasts } from 'react-toast-notifications';
 
 export default function StudentsPage() {
 
+    //toast
+    const { addToast } = useToasts();
+
     const studentState = useSelector( state=> state.student);
     const dispatch = useDispatch();
+    const [deleteModal, showDeleteModal] = React.useState(false);
+
+    const [selectedStudent, setSelectedStudent] = React.useState();
 
     React.useEffect(() => {
 
         dispatch( getStudentsMiddleware());
 
     }, [])
+
+
+    React.useEffect(() => {
+        if (studentState.ACTION_TYPE === deleteStudentSuccess.toString()) {
+
+            addToast("Student deleted successfully", { appearance: 'success' });
+            dispatch( getStudentsMiddleware());
+            showDeleteModal(false)
+
+        }
+
+    }, [studentState.ACTION_TYPE])
+ const deleteUser = ()=>{
+        dispatch(deleteStudentMiddleware(selectedStudent.id));
+    }
     return (
         <>
             <h1>GET ALL STUDENTS</h1>
@@ -25,6 +49,7 @@ export default function StudentsPage() {
                         <th>Full Name</th>
                         <th>Student Email</th>
                         <th>Department</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -34,10 +59,27 @@ export default function StudentsPage() {
                             <td> {student.first_name+" "+student.last_name}</td>
                             <td>{student.email}</td>
                             <td>{student.department}</td>
+                            <th>
+                                <a className="btn btn-primary" href={"/website/edit-student/"+student.id} >Edit</a >
+                                <a className="btn btn-danger" onClick={()=>{
+                                    showDeleteModal(state=> !state);
+                                    setSelectedStudent(student)
+                                }}  >Delete</a >
+                            </th>
                         </tr>
                     )}
                 </tbody>
             </Table>
+            <Modal isOpen={deleteModal}  >
+                <ModalHeader >Delete this student?</ModalHeader>
+                <ModalBody>
+                    Are you sure you want to delete {selectedStudent ?selectedStudent.first_name+" "+ selectedStudent.last_name :null} from the database?
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="danger" onClick={ deleteUser}>{studentState.isDeleteTutorLoading? <Spinner children=""/> : "Yes, Delete"}</Button>{' '}
+                    <Button color="secondary" onClick={()=>showDeleteModal(state=> !state)}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
         </>
     )
 }
